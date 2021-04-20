120da7002b967783c533b359660872bf4c60da88
Andrii Hladkyi <andrii.hladkyi@p2h.com>
2021-04-20 13:11:46 +0300

    Create real api requests

--- lib/rswag/specs/example_helpers.rb
+++ lib/rswag/specs/example_helpers.rb
@@ -1,20 +1,41 @@
 # frozen_string_literal: true
 
+require "rswag/specs/real_request_factory"
 require "rswag/specs/request_factory"
 require "rswag/specs/response_validator"
+require "byebug"
 
 module Rswag
   module Specs
     module ExampleHelpers
       def submit_request(metadata)
         request = RequestFactory.new.build_request(metadata, self)
+        byebug
+        # request = RealRequestFactory.new.build_request(metadata, self)
 
-        send(
-          request[:verb],
-          request[:path],
-          request[:payload],
-          request[:headers]
-        )
+        if metadata[:path_item][:parameters][:api]
+          request = RealRequestFactory.new.build_request(metadata, self)
+        else
+          request = RequestFactory.new.build_request(metadata, self)
+
+          send(
+            request[:verb],
+            request[:path],
+            request[:payload],
+            request[:headers]
+          )
+        end
+
+        # request = metadata[:path_item][:parameters][:api] ?
+        #   RealRequestFactory.new.build_request(metadata, self) :
+        #   RequestFactory.new.build_request(metadata, self)
+
+        # send(
+        #   request[:verb],
+        #   request[:path],
+        #   request[:payload],
+        #   request[:headers]
+        # )
       end
 
       def assert_response_matches_metadata(metadata)
